bccba8940369ad9f99a9bfff0062f91b90fa122e
Akash TM <tm.akash99@gmail.com>
2022-03-29 00:27:19 +0530

    first

---
+++ src/test/java/com/automation/demo/AppTest.java
@@ -0,0 +1,39 @@
+package com.automation.demo;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+   
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     * @return 
+     */
+	@Test
+    public void AppTest2(  )
+    {
+       
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+     @Test
+    public static void suite()
+    {
+      
+    }
+
+    /**
+     * Rigourous Test :-)
+     */@Test
+    public void testApp()
+    {
+       
+    }
+}
---
+++ src/test/java/com/automation/demo/test.java
@@ -0,0 +1,13 @@
+package com.automation.demo;
+
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test
+	public void test1() {
+		
+	}
+	
+
+}
